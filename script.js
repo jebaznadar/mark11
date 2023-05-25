3e570d40c5ad75290a9292f54d4537dcb17dce8c
jebaznadar <126007453+jebaznadar@users.noreply.github.com>
2023-05-25 13:44:53 +0530

    js done

---
+++ script.js
@@ -0,0 +1,38 @@
+const dateOfBirth = document.querySelector("#date-of-birth") ;
+const LuckyNumber = document.querySelector("#lucky-number");
+const CheckButton = document.querySelector("#check-button");
+const outPutBox = document.querySelector("#outputbox");
+
+CheckButton.addEventListener('click',checkBirthDateIsLucky )
+
+function checkBirthDateIsLucky(){
+   const dob = dateOfBirth.value ;
+   const sum = calculateSum(dob)
+   if(sum&&dob){
+    compareValues(sum ,LuckyNumber.value)
+   }
+   else{
+    outPutBox.innerText = "please enter both the fields 😤";
+   }
+   
+
+}
+function calculateSum(dob){
+    dob = dob.replaceAll("-","")
+    let sum = 0 ;
+    for ( let index = 0; index<dob.length;index++){
+         sum = sum + Number(dob.charAt(index));
+    }
+
+
+    return sum; 
+}
+function compareValues(sum , LuckyNumber){
+    outPutBox.style.display = "none;"
+    if(sum%LuckyNumber==0){
+        outPutBox.innerText = "your birthday is Lucky 🥳";
+    }
+    else{
+        outPutBox.innerText = "your birthday is not Lucky 🫤";
+    }
+}
